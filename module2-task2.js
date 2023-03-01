e0e012f05f26b85be784f5473834592150fae328
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2023-03-01 17:38:16 -0600

    Forgot to save changes

--- module2-task2.js
+++ module2-task2.js
@@ -5,6 +5,13 @@ information in the console. Now decrease the number of roses by 20 and lilies by
 in the console again.
 */
 
+/*
+Modify the code from the previous example. Assume that the prices of flowers will be constant (they will not change).
+Declare and initialize the remaining variables in the same way as in the previous example. Display all the gathered
+information in the console. Now decrease the number of roses by 20 and lilies by 30. Display all the collected information
+in the console again.
+*/
+
 const rosePrice = 8;
 const lilyPrice = 10;
 const tulipPrice = 2;
@@ -19,6 +26,24 @@ let tulipsTotal = tulips*tulipPrice;
 
 let total = rosesTotal + liliesTotal + tulipsTotal;
 
+
+console.log("Rose - unit price: " + rosePrice +
+", quantity: " + roses + ", value: " + rosesTotal);
+
+console.log("Lily - unit price: " + lilyPrice +
+", quantity: " + lilies + ", value: " + liliesTotal);
+
+console.log("Tulip - unit price: " + lilyPrice +
+", quantity: " + tulips + ", value: " + tulipsTotal);
+
+console.log("Total: " + total);
+
+roses -= 20;
+lilies -= 30;
+
+rosesTotal = roses*rosePrice;
+liliesTotal = lilies*lilyPrice;
+tulipsTotal = tulips*tulipPrice;
 
 console.log("Rose - unit price: " + rosePrice +
 ", quantity: " + roses + ", value: " + rosesTotal);
